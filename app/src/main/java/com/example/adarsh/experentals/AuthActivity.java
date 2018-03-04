f9b5aad1422411887e2bd4eee07c2e5245ff79e0
AdarshPai97 <paiadarsh.a@gmail.com>
2018-03-04 10:56:04 +0530

    Working2

--- app/src/main/java/com/example/adarsh/experentals/AuthActivity.java
+++ app/src/main/java/com/example/adarsh/experentals/AuthActivity.java
@@ -1,6 +1,7 @@
 package com.example.adarsh.experentals;
 
 import android.app.ProgressDialog;
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
